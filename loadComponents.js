1233beedb57e3d97b225b1e5bb711b827198874a
claudiojas <claudiojasoares.dev@gmail.com>
2025-04-02 03:10:47 -0300

    feat: init commit. Add header component

---
+++ loadComponents.js
@@ -0,0 +1,20 @@
+function loadComponents() {
+    // Carregando o componente de Header
+    fetch('src/header/header.html') 
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o header:', error));
+
+        // Carregando o componete de Footer
+        fetch('src/footer/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o footer:', error));
+}
+
+window.onload = loadComponents;
+
