c35fcad08f625087748f96f47bd43659f12663b3
claudiojas <claudiojasoares.dev@gmail.com>
2025-04-05 20:17:17 -0300

    fix: fix hover effect

--- setupCategoryMenuToggle.js
+++ setupCategoryMenuToggle.js
@@ -4,7 +4,7 @@ export default function setupCategoryMenuHover() {
 
   if (!toggleBtn || !menu) return;
 
-  // Ativa o menu ao passar o mouse sobre o botão
+  // Ativa o menu ao clicar no botão
   toggleBtn.addEventListener("click", () => {
     menu.classList.contains('active') 
       ? menu.classList.remove("active") 
@@ -15,5 +15,16 @@ export default function setupCategoryMenuHover() {
   // Mantém o menu aberto enquanto o mouse estiver sobre ele
   menu.addEventListener("mouseenter", () => {
     menu.classList.add("active");
+  });
+
+  // Fecha o menu quando o mouse sai do botão e do menu
+  toggleBtn.addEventListener("mouseleave", () => {
+    setTimeout(() => {
+      !menu.matches(':hover') &&  menu.classList.remove("active");
+    }, 200);
+  });
+
+  menu.addEventListener("mouseleave", () => {
+    menu.classList.remove("active");
   });
 }
