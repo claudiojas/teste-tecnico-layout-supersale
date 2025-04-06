e347ffdf13904fefb21ed8ae269e8416703aca33
claudiojas <claudiojasoares.dev@gmail.com>
2025-04-06 01:54:53 -0300

    feat: add. effect of the menu all products

--- setupCategoryMenuToggle.js
+++ setupCategoryMenuToggle.js
@@ -1,15 +1,24 @@
 export default function setupCategoryMenuHover() {
   const toggleBtn = document.querySelector(".btn__toggle__menu");
   const menu = document.querySelector(".section__navbar__menu");
+  const icon = document.querySelector(".categorie__item .icon")
 
 
   if (!toggleBtn || !menu) return;
 
   // Ativa o menu ao clicar no botão
   toggleBtn.addEventListener("click", () => {
-    menu.classList.contains('active') 
-      ? menu.classList.remove("active") 
-      : menu.classList.add("active");
+    // menu.classList.contains('active') 
+    //   ? menu.classList.remove("active") 
+    //   : menu.classList.add("active");
+
+    if(menu.classList.contains('active')){
+      menu.classList.remove("active");
+      toggleBtn.style.color = "#000000"
+    } else {
+      menu.classList.add("active");
+      toggleBtn.style.color = "#005CFF"
+    }
 
   });
 
@@ -17,15 +26,4 @@ export default function setupCategoryMenuHover() {
   menu.addEventListener("mouseenter", () => {
     menu.classList.add("active");
   });
-
-  // Fecha o menu quando o mouse sai do botão e do menu
-  // toggleBtn.addEventListener("mouseleave", () => {
-  //   setTimeout(() => {
-  //     !menu.matches(':hover') &&  menu.classList.remove("active");
-  //   }, 200);
-  // });
-
-  // menu.addEventListener("mouseleave", () => {
-  //   menu.classList.remove("active");
-  // });
 }
